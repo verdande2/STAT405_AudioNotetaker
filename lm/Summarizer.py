3a40078514c83e64bd1467b879694db7c67967b4
C041h <jhdonahue41@gmail.com>
2026-02-12 09:23:52 -0800

    dummy summarization class

---
+++ lm/Summarizer.py
@@ -0,0 +1,23 @@
+"""
+dummy summarizer class
+"""
+from llama_cpp import Llama
+
+class summary:
+    def __init__():
+        nothing = 0
+        # path = ""
+        # model_params = 
+        # model = llama_cpp.llama_load_model_from_file(...)
+        # 
+        
+    def SetParameters(): #testing class
+        nothing = 0
+    
+    def SummarizeSingle(transcript):
+        nothing = 0
+        # prompt = 
+        # 
+        
+    def SummarizeSingleWithHistory(transcript, summary):
+        nothing = 0
