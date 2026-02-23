8ac05bf594935f745f39e5d3076b8fc1e179eedf
C041h <jhdonahue41@gmail.com>
2026-02-22 20:13:38 -0800

    initial tuning

--- lm/Summarizer.py
+++ lm/Summarizer.py
@@ -4,43 +4,37 @@ dummy summarizer class
 from llama_cpp import Llama
 from langchain_text_splitters import RecursiveJsonSplitter
 import json
-import ctypes
 
 
 
 class Summary:
     def __init__(self, path): # temporarily empty, will replace the functionality of SetParameters later
-        self.llm = False
-        self.prompt_template = False
-        self.splitter = RecursiveJsonSplitter(max_chunk_size=300)
+        self.splitter = RecursiveJsonSplitter(max_chunk_size=2000, min_chunk_size=1000)
         self.llm = Llama(
             model_path=path,
             n_batch= 512,
             n_context = 512, #change max context size, default 2048
-            tokenizer=(path)
         )
     
-    def SummarizeSingle(self, transcript, temp = 0.2):
+    def SummarizeSingle(self, transcript, temp = 0.5):
         chunks = self.splitter.split_json(json_data=transcript, convert_lists=True)
         summaries = []
         for chunk in chunks:
-            text = ""
+            text = "The following text is a transcript from a patient meeting with a therapist. Summarize the patient's condition and the stories they tell"
             for reply in chunk.values():
                 text = text + reply["speaker"] + ": " + reply["text"] + "\n "
-            inputText = text.encode('utf-8')
-            tokens = self.llm.tokenize(text=inputText)
-            outputTokens = self.llm.generate(tokens, temp=temp)
-            output = self.llm.detokenize(outputTokens)
+            output = self.llm(text, temperature=temp, max_tokens=256) # set max tokens to an amount divisible by amount we want
+            print(output)
             summaries.append(output)
-        
-        #create output variable, change output idk man make it stink dude holy cow everyone plug they nose when u enter the dam room
         return (summaries)
 
 ##########################################################################Bullshit
-with open('lm/testing/sample documents/example1.json', 'r') as f:
+with open('lm/testing/sample documents/example2.json', 'r') as f:
     transcript = json.load(f)
 
-summary = Summary(path='lm/models/Qwen3-4B-Q4_K_M.gguf')
+summary = Summary(path='lm/models/Phi-3-mini-4k-instruct-q4.gguf')
 
-print(summary.SummarizeSingle(transcript=transcript))
+sum = summary.SummarizeSingle(transcript=transcript)
+
+print(sum)
 
