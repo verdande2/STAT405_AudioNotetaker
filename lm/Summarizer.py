be3836e53ee035cb37251dad2f31d29f29990700
C041h <jhdonahue41@gmail.com>
2026-02-22 21:26:20 -0800

    functional full summary untuned

--- lm/Summarizer.py
+++ lm/Summarizer.py
@@ -12,21 +12,30 @@ class Summary:
         self.splitter = RecursiveJsonSplitter(max_chunk_size=2000, min_chunk_size=1000)
         self.llm = Llama(
             model_path=path,
-            n_batch= 512,
-            n_context = 512, #change max context size, default 2048
+            n_batch= 2048,
+            n_context = 2048, #change max context size, default 2048
         )
     
     def SummarizeSingle(self, transcript, temp = 0.5):
         chunks = self.splitter.split_json(json_data=transcript, convert_lists=True)
         summaries = []
         for chunk in chunks:
-            text = "The following text is a transcript from a patient meeting with a therapist. Summarize the patient's condition and the stories they tell"
+            # adjust initial message for better accuracy
+            text = "The following text is a transcript from a patient meeting with a therapist. Summarize the patient's condition and the stories they tell\n "
             for reply in chunk.values():
                 text = text + reply["speaker"] + ": " + reply["text"] + "\n "
-            output = self.llm(text, temperature=temp, max_tokens=256) # set max tokens to an amount divisible by amount we want
-            print(output)
-            summaries.append(output)
-        return (summaries)
+            output = self.llm(text, temperature=temp, max_tokens=100) # set max tokens to an amount divisible by amount we want
+            print(output["choices"][0]["text"])
+            summaries.append(output["choices"][0]["text"])
+        text = "The following text is a collection of summaries from a therapy patient's counseling. Generate a long summary of the patient's experiences\n  "
+        for sum in summaries:
+            text = text + sum
+        print(text)
+        totalOutput = self.llm(text, temperature=temp, max_tokens=1024)
+        print(totalOutput)
+            
+        return (totalOutput)
+
 
 ##########################################################################Bullshit
 with open('lm/testing/sample documents/example2.json', 'r') as f:
